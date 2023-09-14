1f9fd45f68a23b3476ffa2d4810d53cfeefb90d9
Nausher Rao <sherrao15@gmail.com>
2023-09-14 16:34:10 -0400

    Init PNPM configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from "vite";
+/* eslint-disable */
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
