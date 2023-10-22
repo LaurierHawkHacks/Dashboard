fb058d9c067b8a649a5db6addbdb83eb8d81a235
jc <46619361+juancwu@users.noreply.github.com>
2023-10-22 17:23:44 -0400

    remove styles folder

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,6 @@ export default defineConfig({
             "@": path.resolve(__dirname, "src/"),
             "@assets": path.resolve(__dirname, "src/assets/"),
             "@components": path.resolve(__dirname, "src/components/"),
-            "@styles": path.resolve(__dirname, "src/styles/"),
         },
     },
     test: {
