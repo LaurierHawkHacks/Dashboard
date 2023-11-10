6255635be86048c992081c3d2ada864d172d17cc
Chee Kian Teoh <teoh4770@mylaurier.ca>
2023-11-10 18:19:01 -0500

    Finished setup the router

--- src/utils/Router.tsx
+++ src/utils/Router.tsx
@@ -1,10 +1,20 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import LoginPage from "@/pages/LoginPage";
+import App from "@/App";
 
 const routerConfig = createBrowserRouter([
     {
         path: "/",
+        element: <App />,
+    },
+    {
+        path: "/login",
         element: <LoginPage />,
+    },
+    {
+        path: "/admin",
+        element: <h1>Welcome to admin page!!!</h1>,
     },
 ]);
 
