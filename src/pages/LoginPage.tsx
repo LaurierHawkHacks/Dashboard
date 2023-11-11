0dfa050baed3f8bb64645c79e4071237dcaa65ac
Chee Kian Teoh <teoh4770@mylaurier.ca>
2023-11-11 16:28:18 -0500

    Add login page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -3,9 +3,12 @@ import "./LoginPage.css";
 
 const LoginPage = () => {
     return (
-        <>
+        <div className="p-6 space-y-4">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+                Sign in to your account
+            </h1>
             <LoginForm />
-        </>
+        </div>
     );
 };
 
