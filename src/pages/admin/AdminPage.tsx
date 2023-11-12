9d541aee6cf4401683f2b8c3b9ed1d7460cbb8b0
Chee Kian Teoh <teoh4770@mylaurier.ca>
2023-11-12 00:21:42 -0500

    Add an admin page that has a logout button

---
+++ src/pages/admin/AdminPage.tsx
@@ -0,0 +1,20 @@
+import { Button } from "@/components";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "@/providers/AuthProvider";
+
+export const AdminPage = () => {
+    const authProvider = useAuth();
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        authProvider.logout();
+        navigate("/admin/login", { replace: true });
+    };
+
+    return (
+        <div>
+            <h1>Welcome to admin page!!!</h1>
+            <Button onClick={handleLogout}>Sign out</Button>
+        </div>
+    );
+};
