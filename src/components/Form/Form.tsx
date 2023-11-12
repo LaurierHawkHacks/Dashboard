5da109b1b731b736e9c20a697302f7eaa5567000
Chee Kian Teoh <teoh4770@mylaurier.ca>
2023-11-12 00:07:41 -0500

    Delete all incorrect files

--- src/components/Form/Form.tsx
+++
@@ -1,5 +0,0 @@
-type FormProps = React.FormHTMLAttributes<HTMLFormElement>;
-
-export const Form: React.FC<FormProps> = ({ children, ...props }) => {
-    return <form {...props}>{children}</form>;
-};
