1a3cbcac865b6dd039554f406f04509c2dadcd92
Chee Kian Teoh <teoh4770@mylaurier.ca>
2023-11-13 17:21:13 -0500

    Fix the path of the import statement

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 import { Router } from "@utils";
-import { AuthProvider } from "@/providers/AuthProvider";
+import { AuthProvider } from "@providers";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
