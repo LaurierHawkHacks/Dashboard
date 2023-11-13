1cf60741b656a7df8aaa314a8080b36cc2115736
Aidan Traboulay <aidantraboulay13@gmail.com>
2023-11-13 06:49:13 -0500

    Add component exports

---
+++ src/components/index.tsx
@@ -0,0 +1,13 @@
+/*
+ *
+ * @component Button  
+ * 
+ */
+export { Button } from "./Button/Button";
+export { getButtonStyles, type ButtonStylesProps } from "./Button/Button.styles";
+
+/*
+ *
+ * @component 
+ * 
+ */
