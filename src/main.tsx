4fffe7586b22dcedbf3ea268b0d883f1e0726c68
Aidan Traboulay <aidantraboulay13@gmail.com>
2023-11-13 06:48:00 -0500

    Update main

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
+
+import { Router } from "@utils";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <App />
+        <Router />
     </React.StrictMode>
 );
