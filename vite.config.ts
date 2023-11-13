a5486b1585665b0d7859c26278232b66ac795bb7
Chee Kian Teoh <teoh4770@mylaurier.ca>
2023-11-12 21:55:51 -0500

    Add import alias configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,8 @@ export default defineConfig({
             "@assets": path.resolve(__dirname, "src/assets/"),
             "@components": path.resolve(__dirname, "src/components/"),
             "@services": path.resolve(__dirname, "src/services/"),
+            "@utils": path.resolve(__dirname, "src/utils/"),
+            "@providers": path.resolve(__dirname, "src/providers/"),
         },
     },
     test: {
