14995e49970b26d4da562383431034820cbca2f9
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2023-11-19 21:29:03 -0500

    Update: clean up comments

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,38 +3,22 @@
  *
  * import {onCall} from "firebase-functions/v2/https";
  * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
+ * import { onRequest } from "firebase-functions/v2/https";
+ * import * as logger from "firebase-functions/logger";
+ * https://firebase.google.com/docs/functions/typescript
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
-
-// eslint-disable-next-line
-// import { onRequest } from "firebase-functions/v2/https";
-// // eslint-disable-next-line
-// import * as logger from "firebase-functions/logger";
-
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
-
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
-
 admin.initializeApp();
-
 // Default on-sign-up Claims function
 export const addDefaultClaims = functions.auth.user().onCreate(async (user) => {
     const { uid } = user;
-
     try {
         await admin.auth().setCustomUserClaims(uid, {
             // Default Claims
             admin: false, // Example: set to true for admin users
         });
-
         console.log(`Custom claims added for user: ${uid}`);
     } catch (error) {
         console.error("Error adding custom claims:", error);
