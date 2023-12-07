d52ed1429efdf02ad9ca8390e1773146a0832445
amgvu <mingleekevin@gmail.com>
2023-12-06 18:59:27 -0600

    Update applications.ts

--- src/services/applications.ts
+++ src/services/applications.ts
@@ -1,21 +1,35 @@
-import * as admin from "firebase-admin";
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
 
-const apiKey: string = import.meta.env.VITE_FIREBASE_API_KEY;
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
 
-admin.initializeApp({
-    credential: admin.credential.cert(JSON.parse(apiKey)),
-    databaseURL: "https://hawkhacks-dashboard.firebaseapp.com",
-});
+firebase.initializeApp(firebaseConfig);
 
-const db = admin.firestore();
+const db = firebase.firestore();
+const auth = firebase.auth();
 
 export const getApplications = async (
     hackathonTerm: string
 ): Promise<Record<string, unknown>[]> => {
     try {
+        const user = auth.currentUser;
+
+        if (!user) {
+            throw new Error("User not authenticated");
+        }
+
         const querySnapshot = await db
             .collection("applications")
             .where("hackathonTerm", "==", hackathonTerm)
+            .where("userId", "==", user.uid)
             .get();
 
         const applications: Record<string, unknown>[] = [];
