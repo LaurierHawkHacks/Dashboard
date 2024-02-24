066535dad0ec3e7447dafc375c0184cd5d8fa8ee
jc <46619361+juancwu@users.noreply.github.com>
2024-02-24 17:26:45 -0500

    remove Placeholder page component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-export { Placeholder } from "./Placeholder/Placeholder.page";
 export { AdminPage } from "./admin/Admin.page";
 export { UserPage } from "./user/User.page";
 export { LoginPage } from "./Login.page";
