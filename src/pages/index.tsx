2cc422394cbd6f97e3ecc6d21ad8074382a6be1d
jc <46619361+juancwu@users.noreply.github.com>
2024-02-24 03:51:01 -0500

    remove placeholder unnecessary comment/doc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,3 @@
-/*
- *
- * @page Placeholder
- *
- */
 export { Placeholder } from "./Placeholder/Placeholder.page";
 export { AdminPage } from "./admin/Admin.page";
 export { UserPage } from "./user/User.page";
