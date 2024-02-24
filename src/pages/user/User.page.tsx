f41de08cee6aa9f665cbe973f725b1d35844e9af
jc <46619361+juancwu@users.noreply.github.com>
2024-02-24 03:51:35 -0500

    refactor user page

--- src/pages/user/User.page.tsx
+++ src/pages/user/User.page.tsx
@@ -1,20 +1,13 @@
 import { Button } from "@components";
-import { useNavigate } from "react-router-dom";
 import { useAuth } from "@providers";
 
 const UserPage = () => {
-    const authProvider = useAuth();
-    const navigate = useNavigate();
-
-    const handleLogout = () => {
-        authProvider.logout();
-        navigate("/user/login", { replace: true });
-    };
+    const { logout } = useAuth();
 
     return (
         <div>
             <h1>Welcome to user page!!!</h1>
-            <Button onClick={handleLogout}>Sign out</Button>
+            <Button onClick={logout}>Sign out</Button>
         </div>
     );
 };
