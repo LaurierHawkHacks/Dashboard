5ecb082680fc8a89a09422ccf8ae2a9d2b9096d4
amgvu <mingleekevin@gmail.com>
2024-03-08 19:57:14 -0600

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./index.css";
 
 import { Router } from "@utils";
 import { AuthProvider } from "@providers";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <GoogleOAuthProvider clientId="587778980690-d0vll2ts3ubnofb21kre5r3qb6959hfo.apps.googleusercontent.com">
-        <React.StrictMode>
-            <AuthProvider>
-                <Router />
-            </AuthProvider>
-        </React.StrictMode>
-    </GoogleOAuthProvider>,
+    <React.StrictMode>
+        <AuthProvider>
+            <Router />
+        </AuthProvider>
+    </React.StrictMode>,
 );
