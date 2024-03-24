01310471bc3e65f94069b60cad896af848966f99
jc <46619361+juancwu@users.noreply.github.com>
2024-03-24 16:17:48 -0400

    add complete profile page placeholder

---
+++ src/pages/user/CompleteProfile.page.tsx
@@ -0,0 +1,11 @@
+import { PhotoIcon, UserCircleIcon } from "@heroicons/react/24/solid";
+
+export const CompleteProfilePage = () => {
+    return (
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <div className="mx-auto max-w-3xl">
+                <form></form>
+            </div>
+        </div>
+    );
+};
