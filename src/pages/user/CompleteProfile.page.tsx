fc77f7eb5145d48d572980356016d2db5b67108e
jc <46619361+juancwu@users.noreply.github.com>
2024-03-25 23:40:30 -0400

    finalize basic functionality in complete profile page
    there are more fields that need to be added such as proper dropdowns for country, input for phone number, dropdown for school

--- src/pages/user/CompleteProfile.page.tsx
+++ src/pages/user/CompleteProfile.page.tsx
@@ -1,22 +1,93 @@
-import { Button, TextInput } from "@components";
+import { type FormEventHandler, useState } from "react";
+import { z } from "zod";
+import { Navigate } from "react-router-dom";
+import { Button, ErrorAlert, TextInput } from "@components";
+import { type UserProfile, createUserProfile } from "@services/utils";
+import { useAuth, useNotification } from "@providers";
+import { routes } from "@utils";
+
+const countries = ["US", "CA"];
+
+const formValidationSchema = z.object({
+    id: z.string(),
+    firstName: z
+        .string()
+        .min(1, "First name must contain at least 1 character(s)"),
+    lastName: z
+        .string()
+        .min(1, "Last name must contain at least 1 character(s)"),
+    email: z.string().email(),
+    countryOfResidence: z.string().min(2),
+    emailVerified: z.boolean(),
+    phone: z.string(),
+    school: z.string(),
+    levelOfStudy: z.string(),
+});
 
 export const CompleteProfilePage = () => {
+    const { currentUser, userProfile, refreshProfile } = useAuth();
+
+    if (!currentUser) return null;
+
+    if (userProfile) return <Navigate to={routes.profile} />;
+
+    const [formValues, setFormValues] = useState<UserProfile>({
+        id: currentUser.uid,
+        firstName: "",
+        lastName: "",
+        email: currentUser.email ?? "",
+        countryOfResidence: "CA",
+        emailVerified: currentUser.emailVerified,
+        phone: "",
+        school: "",
+        levelOfStudy: "",
+    });
+    const [formErrors, setFormErrors] = useState<string[]>([]);
+
+    const { showNotification } = useNotification();
+
+    const handleSubmit: FormEventHandler = async (e) => {
+        e.preventDefault();
+
+        const results = await formValidationSchema.spa(formValues);
+
+        if (results.success) {
+            await createUserProfile(results.data);
+            showNotification({
+                title: "Profile Completed!",
+                message: "You will be redirect to your portal now!",
+            });
+            await refreshProfile();
+        } else {
+            setFormErrors(results.error.issues.map((i) => i.message));
+        }
+    };
+
     return (
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             <div className="mx-auto max-w-3xl">
-                <h1 className="text-4xl font-semibold leading-7 text-gray-900 py-10">
+                <h1 className="text-4xl font-semibold leading-7 text-transparent bg-clip-text bg-gradient-to-b from-tbrand to-tbrand-highlight mt-10">
                     Complete Your Profile
                 </h1>
-                <form>
+                {formErrors.length > 0 && <ErrorAlert errors={formErrors} />}
+                <form onSubmit={handleSubmit}>
                     <div className="px-4 py-6 sm:p-8">
                         <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                             <div className="sm:col-span-3">
                                 <TextInput
                                     label="First name"
                                     type="text"
-                                    name="first-name"
-                                    id="first-name"
+                                    name="firstName"
+                                    id="firstName"
                                     autoComplete="given-name"
+                                    value={formValues.firstName}
+                                    onChange={(e) =>
+                                        setFormValues((c) => ({
+                                            ...c,
+                                            firstName: e.target.value,
+                                        }))
+                                    }
+                                    required
                                 />
                             </div>
 
@@ -24,21 +95,38 @@ export const CompleteProfilePage = () => {
                                 <TextInput
                                     label="Last name"
                                     type="text"
-                                    name="last-name"
-                                    id="last-name"
+                                    name="lastName"
+                                    id="lastName"
                                     autoComplete="family-name"
+                                    value={formValues.lastName}
+                                    onChange={(e) =>
+                                        setFormValues((c) => ({
+                                            ...c,
+                                            lastName: e.target.value,
+                                        }))
+                                    }
+                                    required
                                 />
                             </div>
 
-                            <div className="sm:col-span-4">
-                                <TextInput
-                                    label="Email address"
-                                    id="email"
-                                    name="email"
-                                    type="email"
-                                    autoComplete="email"
-                                />
-                            </div>
+                            {!currentUser.email && (
+                                <div className="sm:col-span-4">
+                                    <TextInput
+                                        label="Email address"
+                                        id="email"
+                                        name="email"
+                                        type="email"
+                                        value={formValues.email}
+                                        onChange={(e) =>
+                                            setFormValues((c) => ({
+                                                ...c,
+                                                email: e.target.value,
+                                            }))
+                                        }
+                                        required
+                                    />
+                                </div>
+                            )}
 
                             <div className="sm:col-span-4">
                                 <label
@@ -53,10 +141,21 @@ export const CompleteProfilePage = () => {
                                         id="country"
                                         name="country"
                                         autoComplete="country-name"
+                                        value={formValues.countryOfResidence}
+                                        onChange={(e) =>
+                                            setFormValues((c) => ({
+                                                ...c,
+                                                countryOfResidence:
+                                                    e.target.value,
+                                            }))
+                                        }
+                                        required
                                     >
-                                        <option>United States</option>
-                                        <option>Canada</option>
-                                        <option>Mexico</option>
+                                        {countries.map((c) => (
+                                            <option key={c} value={c}>
+                                                {c}
+                                            </option>
+                                        ))}
                                     </select>
                                 </div>
                             </div>
@@ -64,10 +163,7 @@ export const CompleteProfilePage = () => {
                     </div>
                     {/* just a separator line */}
                     <div className="h-0.5 bg-tbrand my-6"></div>
-                    <div className="flex items-center justify-end gap-x-6 px-4 py-4 sm:px-8">
-                        <Button type="reset" intent="secondary">
-                            Clear
-                        </Button>
+                    <div className="flex items-center justify-end px-4 py-4 sm:px-8">
                         <Button type="submit">Save</Button>
                     </div>
                 </form>
