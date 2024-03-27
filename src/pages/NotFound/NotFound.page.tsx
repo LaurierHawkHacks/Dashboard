faacd80828a0b17f9aab75add70c2cc3a4686ff9
jc <46619361+juancwu@users.noreply.github.com>
2024-03-27 11:13:25 -0400

    wrap not found message in quotes
    fixes no escaped entities eslint error

--- src/pages/NotFound/NotFound.page.tsx
+++ src/pages/NotFound/NotFound.page.tsx
@@ -4,8 +4,10 @@ export const NotFoundPage = () => {
     return (
         <div>
             <h1>404 - Page Not Found</h1>
-            <p>The page you're looking for doesn't seem to exist.</p>
-            <Link to="/" className="button">Go Home</Link>
+            <p>{"The page you're looking for doesn't seem to exist."}</p>
+            <Link to="/" className="button">
+                Go Home
+            </Link>
         </div>
     );
 };
