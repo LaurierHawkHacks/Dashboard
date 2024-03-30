16c22b16b166cbd8cda9269738eaaac326102844
Abdul Basit <abdul.b0227@gmail.com>
2024-03-30 17:08:58 -0400

    sidebar responsiveness

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,43 +1,73 @@
+import { GoHome } from "react-icons/go";
+import { PiCalendarCheckFill } from "react-icons/pi";
+import { TiGroup } from "react-icons/ti";
+import { PiIdentificationBadgeFill } from "react-icons/pi";
+import { FiLogOut } from "react-icons/fi";
+import { useState } from "react";
+import { GoHomeFill } from "react-icons/go";
+
 export const Navbar = () => {
+    const [open, setOpen] = useState(true);
+
     return (
-        <div className="w-1/6 bg-white p-4">
-            <div className="flex gap-4 items-center justify-center">
-                <a className="nav-logo" href="/">
+        <div
+            className={`h-screen bg-white transition-all duration-300 gap-12 flex flex-col md:w-[200px] w-[60px]`}
+        >
+            <div>
+                <a className="flex gap-4 items-center justify-center" href="/">
                     <img
-                        className="h-24 w-24"
-                        src="./src/assets/logo.svg"
+                        className="h-12 w-12 mt-6"
+                        src="./src/assets/hh-dashboard-logo.svg"
                         alt="HawkHacks Logo"
                     />
+                    <span className="hidden md:flex">HawkHacks</span>
                 </a>
-                <p>HawkHacks</p>
             </div>
 
-            <aside className="sidebar">
-                <nav className="nav">
-                    <ul className="nav-list">
-                        <li className="nav-item">
-                            <a href="/" className="nav-link">
-                                Home
-                            </a>
-                            
-                        </li>
-                        <li className="nav-item">
-                            <a href="/user" className="nav-link">
-                                Schedule
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a href="/not-found" className="nav-link">
-                                Networking
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a href="/not-found" className="nav-link">
-                                Ticket
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
+            <aside className="flex flex-col items-center justify-between h-full">
+                <ul className="flex flex-col items-start justify-start gap-4">
+                    <li className="flex">
+                        <a
+                            href="/"
+                            className="flex items-center justify-start gap-2"
+                        >
+                            <GoHome size={32} />
+                            <span className="hidden md:flex">Home</span>
+                        </a>
+                    </li>
+                    <li className="flex">
+                        <a
+                            href="/"
+                            className="flex items-center justify-center gap-2"
+                        >
+                            <PiCalendarCheckFill size={32} />
+                            <span className="hidden md:flex">Home</span>
+                        </a>
+                    </li>{" "}
+                    <li className="flex">
+                        <a
+                            href="/"
+                            className="flex items-center justify-center gap-2"
+                        >
+                            <TiGroup size={32} />
+                            <span className="hidden md:flex">Home</span>
+                        </a>
+                    </li>{" "}
+                    <li className="flex">
+                        <a
+                            href="/"
+                            className="flex items-center justify-center gap-2"
+                        >
+                            <PiIdentificationBadgeFill size={32} />
+                            <span className="hidden md:flex">Home</span>
+                        </a>
+                    </li>
+                </ul>
+
+                <div className="flex items-center justify-center gap-2 mb-8">
+                    <FiLogOut size={32} />
+                    <span className="hidden md:flex">Sign out</span>
+                </div>
             </aside>
         </div>
     );
