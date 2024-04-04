2e903ad4cec3c59be6f45f271c8cd7ccbed67c21
jc <46619361+juancwu@users.noreply.github.com>
2024-04-04 17:28:06 -0400

    add new cloud function to verify github email manually

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,6 +10,7 @@
  */
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+import { Octokit } from "octokit";
 admin.initializeApp();
 // Default on-sign-up Claims function
 export const addDefaultClaims = functions.auth.user().onCreate(async (user) => {
@@ -49,3 +50,74 @@ export const addAdminRole = functions.https.onCall((data, context) => {
             return err;
         });
 });
+
+/**
+ * This cloud function is use as a solution to the work around
+ * when signing in with github would lead to unverified email
+ * even if the email has actually been verified with github
+ *
+ * Calls the REST API with octokit to get all the emails the user
+ * has with github and match it with the email we have with firebase auth
+ *
+ * Sends back the email payload that was matched,
+ * refer the link below for more information
+ * https://docs.github.com/en/rest/users/emails?apiVersion=2022-11-28#list-email-addresses-for-the-authenticated-user
+ */
+export const verifyGitHubEmail = functions.https.onCall(
+    async (data, context) => {
+        if (!context.auth) {
+            return new functions.https.HttpsError(
+                "permission-denied",
+                "Not authenticated"
+            );
+        }
+
+        const { token, email } = data;
+
+        if (!token || !email) {
+            return new functions.https.HttpsError(
+                "failed-precondition",
+                "Invalid Payload"
+            );
+        }
+
+        try {
+            const octokit = new Octokit({
+                auth: token,
+            });
+
+            const res = await octokit.request("GET /user/emails", {
+                headers: {
+                    "X-GitHub-Api-Version": "2022-11-28",
+                },
+            });
+
+            if (res.status === 200) {
+                const payloadEmail = res.data.filter(
+                    (data) => data.email === email
+                )[0];
+                if (!payloadEmail)
+                    return new functions.https.HttpsError(
+                        "aborted",
+                        "Fail to match email in payload"
+                    );
+
+                // since we got the email data we need, we check if its verified
+                admin.auth().updateUser(context.auth.uid, {
+                    emailVerified: payloadEmail.verified,
+                });
+                return payloadEmail.verified;
+            } else {
+                return new functions.https.HttpsError(
+                    "unavailable",
+                    "Service unavailable"
+                );
+            }
+        } catch {
+            return new functions.https.HttpsError(
+                "internal",
+                "Failed to verify email"
+            );
+        }
+    }
+);
