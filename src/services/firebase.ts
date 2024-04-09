87caa22ee47ca63b6161351ee62abbc19fba38fd
Jc <46619361+juancwu@users.noreply.github.com>
2024-04-09 14:12:29 -0400

    Revert "AppCheck / ReCaptcha V3"

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,7 +4,6 @@ import { getAnalytics } from "firebase/analytics";
 import { connectAuthEmulator, getAuth } from "firebase/auth";
 import { connectFunctionsEmulator, getFunctions } from "firebase/functions";
 import { connectStorageEmulator, getStorage } from "firebase/storage";
-import { ReCaptchaV3Provider, initializeAppCheck } from "firebase/app-check";
 
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -22,10 +21,6 @@ export const firestore = getFirestore(app);
 export const auth = getAuth(app);
 export const functions = getFunctions(app);
 export const storage = getStorage(app);
-initializeAppCheck(app, {
-    provider: new ReCaptchaV3Provider(import.meta.env.VITE_APP_CHECK_KEY),
-    isTokenAutoRefreshEnabled: true,
-});
 
 // connect to emulators if not in prod
 if (!import.meta.env.PROD) {
