b338714d29df02717b8641f711f8a8403c3834c1
jc <46619361+juancwu@users.noreply.github.com>
2024-04-09 03:28:31 -0400

    add splitVendorChunkPlugin to reduce build size
    build size is still > 800kb after adding the plugin
    next strategy would be refactoring the code and use dynamic importing

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 /// <reference types="vitest" />
-import { defineConfig } from "vite";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-    plugins: [react(), tsconfigPaths()],
+    plugins: [react(), tsconfigPaths(), splitVendorChunkPlugin()],
     test: {
         globals: true,
         environment: "jsdom",
