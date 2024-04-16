fd802ec9461fa8e6a2f4f6b24f9bfe047fa16bc6
jc <46619361+juancwu@users.noreply.github.com>
2024-04-15 20:29:50 -0400

    add post submission page graphics

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -2,6 +2,16 @@ import Logo from "./hh-dashboard-logo.svg";
 import GithubLogo from "./github-mark.svg";
 import GoogleLogo from "./google-logo.svg";
 import AppleLogo from "./apple-logo.svg";
+import CloudRR from "./cloudRR.svg";
+import CloudLL from "./cloudLL.svg";
+import AirBalloon from "./air-balloon.svg";
 
-export { Logo, GithubLogo, GoogleLogo, AppleLogo };
-
+export {
+    Logo,
+    GithubLogo,
+    GoogleLogo,
+    AppleLogo,
+    CloudRR,
+    CloudLL,
+    AirBalloon,
+};
