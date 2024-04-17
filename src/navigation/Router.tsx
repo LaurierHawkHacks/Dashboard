3d65f6c7f9ebafb9af17bfb90f5adf2b17e4072c
jc <46619361+juancwu@users.noreply.github.com>
2024-04-16 21:53:29 -0400

    add verify rsvp page

--- src/navigation/Router.tsx
+++ src/navigation/Router.tsx
@@ -158,6 +158,7 @@ export const Router = () => {
                         path={routes.ticket}
                         element={<TicketPage />}
                     />
+                    <Route path={routes.verifyRSVP} element={<VerifyRSVP />} />
                 </Route>
 
                 {/* Admin Routes */}
