4201e64be913e4fb5b124a1e58ed2644b3c676f8
jc <46619361+juancwu@users.noreply.github.com>
2024-04-20 18:50:11 -0400

    add interface for a normalized cloud function call response

--- src/services/utils/types.ts
+++ src/services/utils/types.ts
@@ -15,3 +15,9 @@ export interface TeamData {
     members: MemberData[]; // the uid of all the members
     teamName: string;
 }
+
+export interface CloudFunctionResponse<T> {
+    status: number;
+    message: string;
+    data: T;
+}
