7ec017387c8ab6c72ae5d611aeac97f357dfb033
AmirAgassi <33383085+AmirAgassi@users.noreply.github.com>
2024-04-26 20:35:34 -0400

    Replace custom URL validation with zod

--- src/components/forms/hackerApplication.ts
+++ src/components/forms/hackerApplication.ts
@@ -12,15 +12,10 @@ import {
     volunteerSpecificOptions,
 } from "@data";
 import type { FormInput } from "./types";
+import * as z from 'zod';
 
 function isValidUrl(url: string) {
-    const pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
-        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
-        '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address (you never know!!)
-        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-        '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-        '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
-    return !!pattern.test(url);
+    return z.string().url().safeParse(url).success;
 }
 
 export const hackerAppFormInputs: FormInput[] = [
