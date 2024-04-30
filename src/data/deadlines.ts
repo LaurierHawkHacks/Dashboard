9fa956f0ed182f5999e8fa4a150e4778c12a1633
Chee Kian Teoh <teoh4770@mylaurier.ca>
2024-04-29 22:15:18 -0400

    add: setup the data for homepage

---
+++ src/data/deadlines.ts
@@ -0,0 +1,10 @@
+const deadlines = {
+    registration: { startDate: "", endDate: "", time: "" },
+    openingCeremony: { startDate: "", endDate: "", time: "" },
+    hackingBegin: { startDate: "", endDate: "", time: "" },
+    hackingEnd: { startDate: "", endDate: "", time: "" },
+    judgingPeriod: { startDate: "", endDate: "", time: "" },
+    closingCeremony: { startDate: "", endDate: "", time: "" },
+};
+
+export { deadlines };
