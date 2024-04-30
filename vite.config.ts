b154d2cf28f4799634494bfb896543fd2bcac3eb
Nausher Rao <SherRao15@gmail.com>
2024-04-30 01:07:20 -0400

    Update prod minor version

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,6 @@ export default defineConfig({
         clearMocks: true,
     },
     define: {
-        APP_VERSION: JSON.stringify("1.3.0"),
+        APP_VERSION: JSON.stringify("1.3.1"),
     },
 });
