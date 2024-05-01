8259b2b5e3fa2994208fb75fe0e542b9135b4aca
jc <46619361+juancwu@users.noreply.github.com>
2024-05-01 13:26:02 -0400

    update home page button + add link to resubmission

--- src/pages/user/User.page.tsx
+++ src/pages/user/User.page.tsx
@@ -1,18 +1,37 @@
+import { Button } from "@/components";
 import { getButtonStyles } from "@/components/Button/Button.styles";
 import { routes } from "@/navigation/constants";
+import { useAuth } from "@/providers/auth.provider";
 import { Link } from "react-router-dom";
 
 const UserPage = () => {
+    const { userApp } = useAuth();
+
     return (
         <>
             <div>
-                <Link
-                    to={routes.application}
-                    className={getButtonStyles({ intent: "primary" })}
-                >
-                    Apply To HawkHacks!
-                </Link>
+                {userApp ? (
+                    <Button disabled={!!userApp}>Apply to HawkHacks!</Button>
+                ) : (
+                    <Link
+                        to={routes.application}
+                        className={getButtonStyles({ intent: "primary" })}
+                    >
+                        Apply To HawkHacks!
+                    </Link>
+                )}
             </div>
+            {userApp && (
+                <p className="mt-4">
+                    Had issues with your application?{" "}
+                    <Link
+                        to={routes.application}
+                        className="font-medium text-sky-600 underline"
+                    >
+                        Resubmit one here.
+                    </Link>
+                </p>
+            )}
         </>
     );
 };
