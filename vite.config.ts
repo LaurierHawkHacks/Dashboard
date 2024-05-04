32f629abd91b68d06fda2c996a0f29f111b738a7
jc <46619361+juancwu@users.noreply.github.com>
2024-05-03 22:38:36 -0400

    v1.6.1 version update

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,6 @@ export default defineConfig({
         clearMocks: true,
     },
     define: {
-        APP_VERSION: JSON.stringify("1.6.0"),
+        APP_VERSION: JSON.stringify("1.6.1"),
     },
 });
