4c5a8d2d5cfb23ea936f5fc3a5324be1fa4c98ab
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-05-04 23:48:19 -0400

    [Add] Dropdown for RSVP Question 1

--- src/pages/miscellaneous/VerifyRSVP.page.tsx
+++ src/pages/miscellaneous/VerifyRSVP.page.tsx
@@ -9,9 +9,12 @@ export const VerifyRSVP = () => {
     const [isVerifying, setIsVerifying] = useState(false);
     const [agreedToParticipate, setAgreedToParticipate] = useState(false);
     const [willAttend, setWillAttend] = useState(false);
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
     const { showNotification } = useNotification();
     const { currentUser, reloadUser } = useAuth();
     const navigate = useNavigate();
+
+    const toggleDropdown = () => setIsDropdownOpen(!isDropdownOpen);
 
     const verify = async () => {
         setIsVerifying(true);
@@ -30,7 +33,7 @@ export const VerifyRSVP = () => {
 
     useEffect(() => {
         if (currentUser && currentUser.rsvpVerified) navigate("/");
-    }, [currentUser]);
+    }, [currentUser, navigate]);
 
     return (
         <div className="pt-12 flex justify-center items-center flex-col gap-6">
@@ -38,22 +41,36 @@ export const VerifyRSVP = () => {
                 Please verify your RSVP to get access to the rest of the
                 dashboard!
             </p>
-            <div className="flex flex-col items-start max-w-2xl">
-                <label className="inline-flex items-center gap-5 mt-3">
-                    <input
-                        type="checkbox"
-                        checked={agreedToParticipate}
-                        onChange={(e) =>
-                            setAgreedToParticipate(e.target.checked)
-                        }
-                        className="form-checkbox h-5 w-5 text-gray-600"
-                    />
-                    <span className="ml-2 text-gray-700">
-                        I have read the details below and agree to participate
-                        in the event using my free will and good judgment.
-                    </span>
-                </label>
-                <label className="inline-flex items-center gap-5 mt-3">
+            <div className="flex flex-col items-start max-w-3xl">
+                <div>
+                    <label className="inline-flex items-center gap-3 mt-3">
+                        <input
+                            type="checkbox"
+                            checked={agreedToParticipate}
+                            onChange={(e) =>
+                                setAgreedToParticipate(e.target.checked)
+                            }
+                            className="form-checkbox h-5 w-5 text-gray-600"
+                        />
+                        <span
+                            onClick={toggleDropdown}
+                            className="ml-2 text-gray-700 cursor-pointer"
+                        >
+                            I have read the content below and agree to
+                            participate in the event using my free will and good
+                            judgment. (Click this text to read)
+                        </span>
+                    </label>
+                    {isDropdownOpen && (
+                        <div className="mt-2 ml-14 text-gray-500">
+                            <p>
+                                I accept full responsibility for my actions for
+                                the entire duration of this event.
+                            </p>
+                        </div>
+                    )}
+                </div>
+                <label className="inline-flex items-center gap-3 mt-3">
                     <input
                         type="checkbox"
                         checked={willAttend}
@@ -67,13 +84,13 @@ export const VerifyRSVP = () => {
                     </span>
                 </label>
             </div>
-            <Button
+            <button
                 onClick={verify}
                 disabled={isVerifying || !agreedToParticipate || !willAttend}
                 className="mt-4 font-bold px-4 py-2 rounded bg-blue-500 text-white disabled:bg-gray-300"
             >
                 Verify
-            </Button>
+            </button>
         </div>
     );
 };
