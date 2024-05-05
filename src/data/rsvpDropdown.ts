1175dece70e838c9809b789081105d9a7e9b9a7b
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-05-04 23:57:50 -0400

    [Create] RSVP data

---
+++ src/data/rsvpDropdown.ts
@@ -0,0 +1,12 @@
+const rsvpDropdown = [
+    "I accept full responsibility for my actions for the entire duration of this event.",
+    "I acknowledge that the Wilfrid Laurier University Students’ Union and its officers, directors, employees, volunteers, and the event organizers are in no way responsible for lost or stolen property, bodily injury or any other misfortune that might come to me as a result of my participation in this event.",
+    "I acknowledge that I will not hold the Wilfrid Laurier University Students’ Union and its officers, directors, employees, volunteers, and the event organizers liable or responsible for any injury or damages sustained from activities related to participation in the Event.",
+    "I acknowledge that there are certain inherent and unavoidable risks in all activities and will always take reasonable steps during this event to avoid placing myself in dangerous and/or potentially hazardous situations.",
+    "I will notify event organizers before the beginning of the event of any health conditions that may be associated with the risk of a medical emergency (e.g. severe allergies).",
+    "In acknowledging the provision of food prepared by external vendors and brought to Wilfrid Laurier University, I understand and accept the inherent risks involved. Despite event organizers' best intentions and efforts to adhere to food safety protocols, I acknowledge the potential for contamination, allergen cross-contact, and mishandling. Recognizing the absence of regulatory oversight of Laurier Food Services and/or Students’ Union Hospitality, I willingly assume any associated risks, including the possibility of foodborne illness or allergic reactions. By choosing to consume such food, I absolve the event organizers, the Wilfrid Laurier Students’ Union and any associated parties of liability and hold them harmless against any adverse outcomes.",
+    "I acknowledge that the event extends over multiple days and nights and that I am responsible for ensuring a safe ride home if I have not had adequate sleep to operate a motor vehicle. I understand that it is my responsibility to arrange transportation or secure accommodation if necessary to ensure my safe return home.",
+    "I hereby grant permission to the event organizers to photograph and video me, and otherwise capture my image, and to make recordings of my voice to be used in marketing materials at the event or location noted above.",
+];
+
+export { rsvpDropdown };
