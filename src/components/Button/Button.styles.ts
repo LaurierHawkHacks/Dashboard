eee160b930142b4e8328187cc7182a2b897aa836
Aidan Traboulay <aidantraboulay13@gmail.com>
2024-05-05 19:49:16 -0400

    Update button styling

--- src/components/Button/Button.styles.ts
+++ src/components/Button/Button.styles.ts
@@ -4,18 +4,18 @@ import { twMerge } from "tailwind-merge";
 
 const buttonStyles = cva(
     [
-        "px-5 py-4 text-sm font-regular transition relative rounded-sm",
+        "px-5 py-4 text-sm font-medium transition relative rounded-sm",
         "focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-tbrand",
-        "disabled:cursor-not-allowed disabled:after:hidden disabled:bg-gray-400 disabled:text-white disabled:hover:bg-gray-400",
+        "disabled:cursor-not-allowed disabled:after:hidden disabled:bg-btn-gradient-disabled disabled:text-white disabled:hover:bg-gray-400",
     ],
     {
         variants: {
             intent: {
                 primary:
-                    "bg-tbrand text-white hover:bg-[#3f9098] active:bg-[#214b4f]",
+                    "bg-btn-gradient text-white hover:bg-[#3f9098] active:bg-[#214b4f]",
                 secondary:
                     "bg-white text-tbrand border border-gray-400 hover:bg-[#ebebeb] disabled:hover:bg-white active:bg-[#d9d9d9]",
-                danger: "bg-white text-red-500 border border-red-500 hover:bg-red-500 hover:text-white disabled:border-none",
+                danger: "bg-btn-gradient-dg text-white hover:bg-white active:bg-[#d9d9d9]",
             },
         },
         defaultVariants: {
