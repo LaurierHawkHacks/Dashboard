1e48a73f5d844a3f78d2a3c08c8d23e617a318ac
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-05-04 23:39:30 -0400

    [Add] Checkboxes and styling

--- src/pages/miscellaneous/VerifyRSVP.page.tsx
+++ src/pages/miscellaneous/VerifyRSVP.page.tsx
@@ -1,12 +1,14 @@
 import { Button } from "@/components";
 import { useAuth } from "@/providers/auth.provider";
-import { useNotification } from "@/providers/notification.provider";
 import { verifyRSVP } from "@/services/utils";
-import { useEffect, useState } from "react";
+import { useNotification } from "@/providers/notification.provider";
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export const VerifyRSVP = () => {
     const [isVerifying, setIsVerifying] = useState(false);
+    const [agreedToParticipate, setAgreedToParticipate] = useState(false);
+    const [willAttend, setWillAttend] = useState(false);
     const { showNotification } = useNotification();
     const { currentUser, reloadUser } = useAuth();
     const navigate = useNavigate();
@@ -27,8 +29,6 @@ export const VerifyRSVP = () => {
     };
 
     useEffect(() => {
-        // we only want to navigate out of the current page if user rsvp was successfully
-        // verified and has been populated in our database
         if (currentUser && currentUser.rsvpVerified) navigate("/");
     }, [currentUser]);
 
@@ -38,10 +38,39 @@ export const VerifyRSVP = () => {
                 Please verify your RSVP to get access to the rest of the
                 dashboard!
             </p>
+            <div className="flex flex-col items-start max-w-2xl">
+                <label className="inline-flex items-center gap-5 mt-3">
+                    <input
+                        type="checkbox"
+                        checked={agreedToParticipate}
+                        onChange={(e) =>
+                            setAgreedToParticipate(e.target.checked)
+                        }
+                        className="form-checkbox h-5 w-5 text-gray-600"
+                    />
+                    <span className="ml-2 text-gray-700">
+                        I have read the details below and agree to participate
+                        in the event using my free will and good judgment.
+                    </span>
+                </label>
+                <label className="inline-flex items-center gap-5 mt-3">
+                    <input
+                        type="checkbox"
+                        checked={willAttend}
+                        onChange={(e) => setWillAttend(e.target.checked)}
+                        className="form-checkbox h-5 w-5 text-gray-600"
+                    />
+                    <span className="ml-2 text-gray-700">
+                        I agree that I will be attending HawkHacks from May 17th
+                        to May 19th. I will try to be on the premises for the
+                        vast majority for the duration of the event.
+                    </span>
+                </label>
+            </div>
             <Button
                 onClick={verify}
-                disabled={isVerifying}
-                className="font-bold"
+                disabled={isVerifying || !agreedToParticipate || !willAttend}
+                className="mt-4 font-bold px-4 py-2 rounded bg-blue-500 text-white disabled:bg-gray-300"
             >
                 Verify
             </Button>
