02f8101985e76feead251b2c2289bb7800405dd1
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-05-04 23:57:50 -0400

    [Create] RSVP data

--- src/data/index.ts
+++ src/data/index.ts
@@ -8,6 +8,7 @@ export { cityNames } from "./cities";
 export { faqs } from "./faqs";
 export { socialLinks } from "./SocialLinks";
 export { sponsors } from "./sponsors";
+export { rsvpDropdown } from "./rsvpDropdown";
 
 export const genders = [
     "Man",
