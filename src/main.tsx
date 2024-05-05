9a8ded8426808704fbef38145ce690eddcbd19eb
jc <46619361+juancwu@users.noreply.github.com>
2024-05-04 23:47:00 -0400

    update

--- src/main.tsx
+++ src/main.tsx
@@ -12,6 +12,7 @@ console.log("If you found this, you are a curious one! 😄");
 console.log("BUT we are not hiring at the moment ^_^");
 console.log(`App version: ${APP_VERSION}`);
 console.log(`App env: ${import.meta.env.MODE}`);
+console.log("update");
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
