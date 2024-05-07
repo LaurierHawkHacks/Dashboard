0a5f770cf91d1669719b015b7ec6d2d548b5cbb2
Chee Kian Teoh <teoh4770@mylaurier.ca>
2024-05-07 19:44:23 -0400

    add: update the important date time data

---
+++ src/data/importantDateTimes.ts
@@ -0,0 +1,22 @@
+const importantDateTimes = [
+    {
+        date: "May 17, 2024",
+        label: "First Day",
+        events: {
+            "Registration Time": "6:00 PM - 11:00 PM",
+            "Opening Ceremony": "7:45 PM - 9:20 PM",
+            "Hacking Begin": "9:20 PM",
+        },
+    },
+    {
+        date: "May 19, 2024",
+        label: "Last Day",
+        events: {
+            "Hacking End": "10:00 AM",
+            "Judging Period": "11:00 AM - 3:00 PM",
+            "Closing Ceremony": "4:00 PM - 5:00 PM",
+        },
+    },
+];
+
+export { importantDateTimes };
