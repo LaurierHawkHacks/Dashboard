7fc749d1f565eee81a94f13f3d30de368dfd558a
jc <46619361+juancwu@users.noreply.github.com>
2024-05-08 17:09:43 -0400

    show routes to volunteer

--- src/providers/routes.provider.tsx
+++ src/providers/routes.provider.tsx
@@ -285,7 +285,8 @@ export const RoutesProvider: FC<ComponentProps> = ({ children }) => {
         if (
             currentUser.type === "speaker" ||
             currentUser.type === "sponsor" ||
-            (currentUser.type === "mentor" && userApp && userApp.accepted)
+            (currentUser.type === "mentor" && userApp && userApp.accepted) ||
+            (currentUser.type === "volunteer" && userApp && userApp.accepted)
         ) {
             userRoutes.children = [
                 {
