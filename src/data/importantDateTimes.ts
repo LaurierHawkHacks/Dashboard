2fae080c68eadfdf952f1996776ed66f6399ff2b
Chee Kian Teoh <98545971+teoh4770@users.noreply.github.com>
2024-05-07 21:20:44 -0400

    fix: update importantDateTimes data

--- src/data/importantDateTimes.ts
+++ src/data/importantDateTimes.ts
@@ -5,14 +5,14 @@ const importantDateTimes = [
         events: {
             "Registration Time": "6:00 PM - 11:00 PM",
             "Opening Ceremony": "7:45 PM - 9:20 PM",
-            "Hacking Begin": "9:20 PM",
+            "Hacking Begins": "9:20 PM",
         },
     },
     {
         date: "May 19, 2024",
         label: "Last Day",
         events: {
-            "Hacking End": "10:00 AM",
+            "Hacking Ends": "10:00 AM",
             "Judging Period": "11:00 AM - 3:00 PM",
             "Closing Ceremony": "4:00 PM - 5:00 PM",
         },
