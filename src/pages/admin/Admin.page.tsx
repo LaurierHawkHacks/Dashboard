0d1ae322ac371c44c2189d1168aead9eba8bbf9f
jc <46619361+juancwu@users.noreply.github.com>
2024-05-08 18:28:31 -0400

    remove sign out button from main admin page

--- src/pages/admin/Admin.page.tsx
+++ src/pages/admin/Admin.page.tsx
@@ -1,13 +1,7 @@
-import { Button } from "@components";
-import { useAuth } from "@/providers/hooks";
-
 export const AdminPage = () => {
-    const { logout } = useAuth();
-
     return (
         <div>
             <h1>Welcome to admin page!!!</h1>
-            <Button onClick={logout}>Sign out</Button>
         </div>
     );
 };
