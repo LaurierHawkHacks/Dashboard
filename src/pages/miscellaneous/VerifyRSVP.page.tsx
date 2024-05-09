985335be55a6861f346eb6367b6e0e05c66c6cfb
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-05-08 23:17:20 -0400

    [Update] configure LoadingAnimation

--- src/pages/miscellaneous/VerifyRSVP.page.tsx
+++ src/pages/miscellaneous/VerifyRSVP.page.tsx
@@ -26,8 +26,8 @@ export const VerifyRSVP = () => {
             });
         } else {
             await reloadUser();
+            setIsVerifying(false);
         }
-        setIsVerifying(false);
     };
 
     useEffect(() => {
@@ -35,74 +35,81 @@ export const VerifyRSVP = () => {
     }, [currentUser, navigate]);
 
     return (
-        <div className="pt-12 flex justify-center items-center flex-col gap-6">
-            <p className="text-lg font-bold">
-                Please verify your RSVP to get access to the rest of the
-                dashboard!
-            </p>
-            <div className="flex flex-col items-start max-w-3xl">
-                <div>
-                    <label className="inline-flex items-center gap-3 mt-3">
-                        <input
-                            type="checkbox"
-                            checked={agreedToParticipate}
-                            onChange={(e) =>
-                                setAgreedToParticipate(e.target.checked)
-                            }
-                            className="form-checkbox h-5 w-5 text-gray-600"
-                        />
-                        <span className="ml-2 text-gray-700 cursor-pointer">
-                            I have read the content below and agree to
-                            participate in the event using my free will and good
-                            judgment.
-                        </span>
-                    </label>
-
-                    <div className="border border-blueGreen p-2 my-8 flex flex-col gap-2 text-gray-500 max-w-3xl max-h-72 overflow-y-scroll">
-                        {rsvpText.map((content, index) => {
-                            return <p key={index}>{content}</p>;
-                        })}
-                    </div>
-                </div>
-                <label className="inline-flex items-center gap-3 mt-3">
-                    <input
-                        type="checkbox"
-                        checked={willAttend}
-                        onChange={(e) => setWillAttend(e.target.checked)}
-                        className="form-checkbox h-5 w-5 text-gray-600"
-                    />
-                    <span className="ml-2 text-gray-700">
-                        I confirm that I will be attending HawkHacks from May
-                        17th to May 19th. I will try to be on the premises for
-                        the vast majority for the duration of the event.
-                    </span>
-                </label>
-            </div>
+        <>
             {isVerifying ? (
-                <LoadingAnimation />
+                <LoadingAnimation text="Loading . . ." />
             ) : (
-                <Button
-                    onClick={verify}
-                    disabled={
-                        isVerifying || !agreedToParticipate || !willAttend
-                    }
-                    className="mt-4 font-bold px-4 py-2 rounded bg-blue-500 text-white disabled:bg-gray-300"
-                >
-                    Verify
-                </Button>
+                <div className="pt-12 flex justify-center items-center flex-col gap-6">
+                    <p className="text-lg font-bold">
+                        Please verify your RSVP to get access to the rest of the
+                        dashboard!
+                    </p>
+                    <div className="flex flex-col items-start max-w-3xl">
+                        <div>
+                            <label className="inline-flex items-center gap-3 mt-3">
+                                <input
+                                    type="checkbox"
+                                    checked={agreedToParticipate}
+                                    onChange={(e) =>
+                                        setAgreedToParticipate(e.target.checked)
+                                    }
+                                    className="form-checkbox h-5 w-5 text-gray-600"
+                                />
+                                <span className="ml-2 text-gray-700 cursor-pointer">
+                                    I have read the content below and agree to
+                                    participate in the event using my free will
+                                    and good judgment.
+                                </span>
+                            </label>
+
+                            <div className="border border-blueGreen p-2 my-8 flex flex-col gap-2 text-gray-500 max-w-3xl max-h-72 overflow-y-scroll">
+                                {rsvpText.map((content, index) => {
+                                    return <p key={index}>{content}</p>;
+                                })}
+                            </div>
+                        </div>
+                        <label className="inline-flex items-center gap-3 mt-3">
+                            <input
+                                type="checkbox"
+                                checked={willAttend}
+                                onChange={(e) =>
+                                    setWillAttend(e.target.checked)
+                                }
+                                className="form-checkbox h-5 w-5 text-gray-600"
+                            />
+                            <span className="ml-2 text-gray-700">
+                                I confirm that I will be attending HawkHacks
+                                from May 17th to May 19th. I will try to be on
+                                the premises for the vast majority for the
+                                duration of the event.
+                            </span>
+                        </label>
+                    </div>
+
+                    <Button
+                        onClick={verify}
+                        disabled={
+                            isVerifying || !agreedToParticipate || !willAttend
+                        }
+                        className="mt-4 font-bold px-4 py-2 rounded bg-blue-500 text-white disabled:bg-gray-300"
+                    >
+                        Verify
+                    </Button>
+
+                    <p className="text-gray-800 mt-2">
+                        Having trouble? Get help in our{" "}
+                        <a
+                            href="https://discord.com/invite/GxwvFEn9TB"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="text-sky-600 font-bold underline"
+                        >
+                            Discord
+                        </a>{" "}
+                        support channel.
+                    </p>
+                </div>
             )}
-            <p className="text-gray-800 mt-2">
-                Having trouble? Get help in our{" "}
-                <a
-                    href="https://discord.com/invite/GxwvFEn9TB"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-sky-600 font-bold underline"
-                >
-                    Discord
-                </a>{" "}
-                support channel.
-            </p>
-        </div>
+        </>
     );
 };
