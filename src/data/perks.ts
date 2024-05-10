87da92a01df6b801cb79a2aa7f02f525122f5263
AmirAgassi <amiragassi04@gmail.com>
2024-05-10 01:24:59 -0400

    Create perks.ts

---
+++ src/data/perks.ts
@@ -0,0 +1,92 @@
+const perksData = {
+    food: [
+        {
+            image: '/src/assets/perks/fantaun.png',
+            title: 'Fantuan - FREE BUBBLE TEA',
+            description: 'Fantuan is providing all of our participants with free bubble tea! All you\'ll have to do to secure some is place a FREE order on the HawkHacks Fantuan store!',
+            link: 'https://order.fantuan.ca/store/Restaurant/ca-22098?shippingType=0&inviteCode=ca09jcqrt&tTraceId=MerchantShare-22098-0-68c98f2f08c048e995841671731611b3&channel=merchant_share',
+            alt: 'Fantuan Delivery'
+        },
+        {
+            image: '/src/assets/perks/Indofood.svg',
+            title: 'Indomie - Get Your Noodle Fix!',
+            description: 'Indomie has graciously provided all hackers 1 cup noodle (feel free to cook some up during HawkHacks 😉) and 1 pillow pack of ramen. Hackers will have the opportunity to pick up their cup noodle Friday night as a late-night snack, and the pillow pack of ramen will be provided once you check-in at registration.',
+            link: null,
+            alt: 'Indomie'
+        },
+        {
+            image: '/src/assets/perks/smokes_Poutinerie.jpg',
+            title: 'Smokes - How Do You Like Your Poutine?',
+            description: 'Smokes will be offering various prizes throughout the duration of the HawkHacks weekend, such as stickers, merch, and other goodies!',
+            link: 'https://smokespoutinerie.com/?utm_medium=affiliate&utm_term=&utm_content&utm_campaign=hawkhacks24',
+            alt: 'Smokes Poutinerie'
+        }
+    ],
+    other: [
+        {
+            image: '/src/assets/perks/1password.svg',
+            title: '1Password - Hassle-Free Security!',
+            description: '1Password has graciously provided all hackers who attend HawkHacks 1 year free of 1Password Families! (note: offer is valid for new signups only)',
+            link: 'https://1password.com',
+            alt: '1Password'
+        },
+        {
+            image: '/src/assets/perks/Balsamiq.png',
+            title: 'Balsamiq - The Low-Fidelity, High-Impact tool!',
+            description: 'Balsamiq will be providing all HawkHack attendees 60-day extended trail of Balsamiq Cloud. Balsamiq Cloud is an effortless wireframing tool, a low-fidelity, high-impact tool highly valued by product managers, founders, developers, & UX teams.',
+            link: 'https://balsamiq.com/?utm_medium=affiliate&utm_term=&utm_content&utm_campaign=hawkhacks24',
+            alt: 'Balsamiq'
+        },
+        {
+            image: '/src/assets/perks/Wolfram.svg',
+            title: 'Wolfram - Computational Intelligence For You!',
+            description: 'Wolfram will be providing the top six teams the Wolfram Award, regardless to whether solutions include Wolfram Tech. This award provides a year of Wolfram|One Personal Edition, as well as a one-year subscription to Wolfram|Alpha Pro. Additionally, Wolfram will be giving all HawkHack participants access to Wolfram|One for 30 days.',
+            link: 'https://www.wolframalpha.com/?utm_medium=affiliate&utm_term=&utm_content&utm_campaign=hawkhacks24',
+            alt: 'Wolfram'
+        },
+        {
+            image: '/src/assets/perks/Echo3D.webp',
+            title: 'Echo3D - Take Control Over Your 3D Content!',
+            description: 'Echo3D will give all HawkHack participants a 1-month Pro tier subscription. Recorded workshop about 3D/AR/VR and how to use echo3D, as well as tech support via Discord will be available to hackers during the weekend.',
+            link: 'https://www.echo3d.com/?utm_medium=affiliate&utm_term=&utm_content&utm_campaign=hawkhacks24',
+            alt: 'Echo3D'
+        },
+        {
+            image: '/src/assets/perks/Taskade.png',
+            title: 'Taskade - AI-Powered Productivity!',
+            description: 'Taskade will provide 40 HawkHack winners with 3-year upgrades to Taskade\'s starter plan. Taskade will give all HawkHack particpants promo codes for 20% off their plans.',
+            link: null,
+            alt: 'Taskade'
+        },
+        {
+            image: '/src/assets/perks/Rosenfeld.png',
+            title: 'Rosenfeld - Resources For All!',
+            description: 'Rosenfeld will give 20% discount promos when checking out using the provided link for all HawkHacks attendees.',
+            link: 'https://rosenfeldmedia.com/?utm_medium=affiliate&utm_term=&utm_content&utm_campaign=hawkhacks24',
+            alt: 'Rosenfeld'
+        },
+        {
+            image: '/src/assets/perks/Certopus.svg',
+            title: 'Certopus - Digital Certificates & Badges!',
+            description: 'Certopus will be providing free-verified e-certificates for all HawkHack attendees who complete their courses.',
+            link: 'https://rosenfeldmedia.com/?utm_medium=affiliate&utm_term=&utm_content&utm_campaign=hawkhacks24',
+            alt: 'Certopus'
+        },
+        {
+            image: '/src/assets/perks/NordVPN_horizontal.png',
+            title: 'NordVPN & NordPass - Be More Secure!',
+            description: 'NordVPN has collaborated with HawkHacks to provide the top 16 victors a full year\'s access to NordVPN and NordPass.',
+            link: 'https://nordvpn.com/hackathons/?utm_medium=affiliate&utm_term=&utm_content&utm_campaign=hawkhacks24',
+            alt: 'NordVPN & NordPass'
+        },
+        {
+            image: '/src/assets/perks/incogni_black.png',
+            title: 'Incogni - Take Back Control of Your Data!',
+            description: 'Incogni will award our top 16 hackers with one-year subscriptions to their product, Incogni. This product empowers users to regain control of their data, minimize spam, and thwart scam attempts by seamlessly opting them out of data broker databases.',
+            link: null,
+            alt: 'Incogni'
+        }
+    ]
+};
+
+export { perksData };
