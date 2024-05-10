8a9774371e8a10a996af0fdb0eecad513da22c32
jc <46619361+juancwu@users.noreply.github.com>
2024-05-10 01:36:45 -0400

    finish up schedule page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ import forms from "@tailwindcss/forms";
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ["./index.html", "./src/**/*.{ts,tsx}"],
+    important: true,
     theme: {
         extend: {
             fontFamily: {
