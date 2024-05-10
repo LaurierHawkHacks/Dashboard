990a195d9a38a8cdb5133e0caf468baa6c297d7a
AmirAgassi <amiragassi04@gmail.com>
2024-05-10 01:44:32 -0400

    Remove quick test SVG

--- src/pages/Perks/Perks.page.tsx
+++ src/pages/Perks/Perks.page.tsx
@@ -98,15 +98,6 @@ const PerksPage = () => {
               className="absolute top-2 right-2 text-gray-600 hover:text-gray-800 focus:outline-none"
               onClick={closePopup}
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-              </svg>
             </button>
             <div className="flex items-center mb-6">
               <div className="w-20 h-20 mr-6 flex items-center justify-center">
