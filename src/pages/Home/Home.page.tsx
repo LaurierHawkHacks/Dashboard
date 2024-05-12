769e35c35270298f4c00f27cf3ee8dc89ef1f39b
jc <46619361+juancwu@users.noreply.github.com>
2024-05-12 02:06:25 -0400

    add confirmation modal before dismissing RSVP

--- src/pages/Home/Home.page.tsx
+++ src/pages/Home/Home.page.tsx
@@ -1,7 +1,8 @@
 import { GoldenHawk, IpadKidHawks } from "@/assets";
-import { Card, Accordion, SocialIcons } from "@components";
+import { Card, Accordion, SocialIcons, Button, Modal } from "@components";
 import { faqs, sponsors, importantDateTimes } from "@data";
 import { useAuth } from "@/providers/auth.provider";
+import { useState } from "react";
 
 const ImportantInfoBlocks = importantDateTimes.map((importantDateTime, i) => {
     const entries = Object.entries(importantDateTime.events);
@@ -46,118 +47,146 @@ const Sponsors = sponsors.map((sponsor, i) => {
 
 const HomePage = () => {
     const { currentUser } = useAuth();
+    const [disableAllActions, setDisableAllActions] = useState(false);
+    const [openDismissRSVPWarning, setOpenDismissRSVPWarning] = useState(false);
 
     return (
-        <section className="homepage grid gap-4">
-            <div className="grid xl:grid-cols-12 gap-4">
-                <Card
-                    title="What is HawkHacks?"
-                    className="about xl:col-span-7"
-                >
-                    <div className="flex text-[#333] text-sm">
-                        <p className="max-w-[37.5rem]">
-                            HawkHacks came out of a desire to give everyone an
-                            equal opportunity to get into tech, whether that be
-                            programming, networking, researching, learning, or
-                            teaching.
-                            <br />
-                            <br />
-                            Join hundreds of students across Canada (and across
-                            the world) in a 36 hour period of exploration,
-                            creativity, and learning!
-                            <br />
-                            <br />
-                            Remember, you don’t have to be a pro to participate
-                            - show up with ten years or ten minutes of
-                            experience (oh yeah, and a great attitute too!)
-                        </p>
-                        <img
-                            className="hidden sm:block md:hidden lg:block -translate-y-4"
-                            src={GoldenHawk}
-                            alt=""
-                        />
-                    </div>
-                </Card>
+        <>
+            <section className="homepage grid gap-4">
+                <div className="grid xl:grid-cols-12 gap-4">
+                    <Card
+                        title="What is HawkHacks?"
+                        className="about xl:col-span-7"
+                    >
+                        <div className="flex text-[#333] text-sm">
+                            <p className="max-w-[37.5rem]">
+                                HawkHacks came out of a desire to give everyone
+                                an equal opportunity to get into tech, whether
+                                that be programming, networking, researching,
+                                learning, or teaching.
+                                <br />
+                                <br />
+                                Join hundreds of students across Canada (and
+                                across the world) in a 36 hour period of
+                                exploration, creativity, and learning!
+                                <br />
+                                <br />
+                                Remember, you don’t have to be a pro to
+                                participate - show up with ten years or ten
+                                minutes of experience (oh yeah, and a great
+                                attitute too!)
+                            </p>
+                            <img
+                                className="hidden sm:block md:hidden lg:block -translate-y-4"
+                                src={GoldenHawk}
+                                alt=""
+                            />
+                        </div>
+                    </Card>
 
-                <Card title="RSVP Status" className="xl:col-span-5">
-                    <span className="flex flex-col gap-2">
-                        <p className="text-[#333] text-sm">
-                            RSVP status:{" "}
-                            <b>
-                                {currentUser?.rsvpVerified
-                                    ? "RSVP'd"
-                                    : "Not RSVP'd"}
-                            </b>
-                        </p>
-                    </span>
-                </Card>
+                    <Card title="RSVP Status" className="xl:col-span-5">
+                        <span className="flex flex-col gap-2">
+                            <p className="text-[#333] text-sm">
+                                RSVP status:{" "}
+                                <b>
+                                    {currentUser?.rsvpVerified
+                                        ? "RSVP'd"
+                                        : "Not RSVP'd"}
+                                </b>
+                            </p>
+                        </span>
+                        <Button
+                            onClick={() =>
+                                !disableAllActions &&
+                                setOpenDismissRSVPWarning(true)
+                            }
+                            intent="secondary"
+                            className="rounded-lg mt-4 border-gray-300"
+                        >
+                            Not able to make it?
+                        </Button>
+                    </Card>
 
-                <Card
-                    title="Important Information"
-                    className="infos xl:col-span-5"
-                >
-                    <div className="text-[#333] text-sm space-y-6">
-                        {ImportantInfoBlocks}
-                    </div>
-                </Card>
+                    <Card
+                        title="Important Information"
+                        className="infos xl:col-span-5"
+                    >
+                        <div className="text-[#333] text-sm space-y-6">
+                            {ImportantInfoBlocks}
+                        </div>
+                    </Card>
 
-                <Card
-                    title="Need help? Contact us!"
-                    className="contacts xl:col-span-5"
-                >
-                    <div className="flex gap-2">
-                        <p className="text-[#333] text-sm lg:max-w-[37.5rem]">
-                            Reach out at{" "}
-                            <a href="mailto:hello@hawkhacks.ca">
-                                hello@hawkhacks.ca
-                            </a>{" "}
-                            for any help or support, and please be sure to join
-                            the HawkHacks Discord community!
-                        </p>
-                        <img
-                            className="hidden sm:block"
-                            src={IpadKidHawks}
-                            alt=""
-                        />
-                    </div>
-
-                    <SocialIcons className="mt-8" />
-                </Card>
-
-                <Card
-                    title="Our Sponsors"
-                    className="grid sponsors xl:col-span-7"
-                >
-                    <div className="text-[#333] text-sm">
-                        <p>
-                            Thank you to all our sponsors! Without them,
-                            HawkHacks could not have happened!
-                            <br />
-                            <br />
-                            Here are some of our top sponsors that we like to
-                            thank:
-                        </p>
-
-                        <div className="sponsors flex flex-wrap gap-x-4 gap-y-2 p-4">
-                            {Sponsors}
+                    <Card
+                        title="Need help? Contact us!"
+                        className="contacts xl:col-span-5"
+                    >
+                        <div className="flex gap-2">
+                            <p className="text-[#333] text-sm lg:max-w-[37.5rem]">
+                                Reach out at{" "}
+                                <a href="mailto:hello@hawkhacks.ca">
+                                    hello@hawkhacks.ca
+                                </a>{" "}
+                                for any help or support, and please be sure to
+                                join the HawkHacks Discord community!
+                            </p>
+                            <img
+                                className="hidden sm:block"
+                                src={IpadKidHawks}
+                                alt=""
+                            />
                         </div>
 
-                        <a
-                            className="mt-auto hover:underline font-bold"
-                            href="https://hawkhacks.ca/"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            Check out all our sponsors! ✨
-                        </a>
-                    </div>
-                </Card>
-            </div>
+                        <SocialIcons className="mt-8" />
+                    </Card>
 
-            <Card title="FAQ" className="faq">
-                <Accordion faqs={faqs} />
-            </Card>
-        </section>
+                    <Card
+                        title="Our Sponsors"
+                        className="grid sponsors xl:col-span-7"
+                    >
+                        <div className="text-[#333] text-sm">
+                            <p>
+                                Thank you to all our sponsors! Without them,
+                                HawkHacks could not have happened!
+                                <br />
+                                <br />
+                                Here are some of our top sponsors that we like
+                                to thank:
+                            </p>
+
+                            <div className="sponsors flex flex-wrap gap-x-4 gap-y-2 p-4">
+                                {Sponsors}
+                            </div>
+
+                            <a
+                                className="mt-auto hover:underline font-bold"
+                                href="https://hawkhacks.ca/"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                Check out all our sponsors! ✨
+                            </a>
+                        </div>
+                    </Card>
+                </div>
+
+                <Card title="FAQ" className="faq">
+                    <Accordion faqs={faqs} />
+                </Card>
+            </section>
+            <Modal
+                title="Dismissing RSVP!"
+                subTitle="You will have to get into the waitlist to RSVP again."
+                open={openDismissRSVPWarning}
+                onClose={() =>
+                    !disableAllActions && setOpenDismissRSVPWarning(false)
+                }
+            >
+                <div className="flex gap-4">
+                    <Button>Nah, I think I will make it somehow</Button>
+                    <Button intent="secondary">Dismiss</Button>
+                </div>
+            </Modal>
+        </>
     );
 };
 
