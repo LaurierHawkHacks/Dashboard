071239d892958c3e86ed47e6a638c0935f2249bc
Aidan Traboulay <aidantraboulay13@gmail.com>
2024-05-11 21:05:30 -0400

    Update metis link

--- src/data/sponsorLinks.ts
+++ src/data/sponsorLinks.ts
@@ -7,7 +7,7 @@ const sponsorLinks = {
     distributive: "https://distributive.network/?utm_medium=affiliate&utm_term=&utm_content&utm_campaign=hawkhacks24",
     sp: "https://smokespoutinerie.com/?utm_medium=affiliate&utm_term=&utm_content&utm_campaign=hawkhacks24",
     neurelo: "https://www.neurelo.com/?utm_medium=affiliate&utm_term=&utm_content&utm_campaign=hawkhacks24",
-    metis: "https://www.metis.io/",
+    metis: "https://metis.io/?utm_medium=affiliate&utm_term=&utm_content&utm_campaign=hawkhacks24/",
     fantuan: "https://fantuan.ca/delivery/en/?utm_medium=affiliate&utm_term=&utm_content&utm_campaign=hawkhacks24",
     dominos: "https://www.dominos.ca/en/?utm_medium=affiliate&utm_term=&utm_content&utm_campaign=hawkhacks24",
 };
