ad9e66231e10f3ff6e17684eb472e2a266c77747
jc <46619361+juancwu@users.noreply.github.com>
2024-05-12 18:33:50 -0400

    v2.1.1 patch update

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,6 @@ export default defineConfig({
         clearMocks: true,
     },
     define: {
-        APP_VERSION: JSON.stringify("2.1.0"),
+        APP_VERSION: JSON.stringify("2.1.1"),
     },
 });
