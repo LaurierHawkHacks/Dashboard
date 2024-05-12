0ae94d24cd891683bbffdae4749e0e7aa6f76b6e
Aidan Traboulay <aidantraboulay13@gmail.com>
2024-05-11 21:02:44 -0400

    Update sponsor links

--- src/data/sponsorLinks.ts
+++ src/data/sponsorLinks.ts
@@ -4,10 +4,12 @@ const sponsorLinks = {
     near: "https://near.org/?utm_medium=affiliate&utm_term=&utm_content&utm_campaign=hawkhacks24",
 
     // gold
-    fantuan: "https://fantuan.ca/delivery/en/?utm_medium=affiliate&utm_term=&utm_content&utm_campaign=hawkhacks24",
-    sp: "https://smokespoutinerie.com/?utm_medium=affiliate&utm_term=&utm_content&utm_campaign=hawkhacks24",
-    domino: "https://www.dominos.ca/en/?utm_medium=affiliate&utm_term=&utm_content&utm_campaign=hawkhacks24",
     distributive: "https://distributive.network/?utm_medium=affiliate&utm_term=&utm_content&utm_campaign=hawkhacks24",
+    sp: "https://smokespoutinerie.com/?utm_medium=affiliate&utm_term=&utm_content&utm_campaign=hawkhacks24",
+    neurelo: "https://www.neurelo.com/?utm_medium=affiliate&utm_term=&utm_content&utm_campaign=hawkhacks24",
+    metis: "https://www.metis.io/",
+    fantuan: "https://fantuan.ca/delivery/en/?utm_medium=affiliate&utm_term=&utm_content&utm_campaign=hawkhacks24",
+    dominos: "https://www.dominos.ca/en/?utm_medium=affiliate&utm_term=&utm_content&utm_campaign=hawkhacks24",
 };
 
 export { sponsorLinks };
