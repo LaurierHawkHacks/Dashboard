3b4e130306e3874d1567a629e04d89a531a619f8
Aidan Traboulay <aidantraboulay13@gmail.com>
2024-05-12 20:56:21 -0400

    Update loading message

--- src/components/LoadingAnimation.tsx
+++ src/components/LoadingAnimation.tsx
@@ -6,15 +6,20 @@ interface LoadingAnimationProps {
 
 export const LoadingAnimation: React.FC<LoadingAnimationProps> = ({ text }) => {
     return (
-        <div className="flex h-screen items-center justify-center">
-            <p className="text-lg md:text-2xl">
-                {text || "Loading super awesome portal..."}
-            </p>
+        <div className="flex h-screen items-center justify-center flex-col">
             <img
                 className="w-12 h-12 md:w-16 md:h-16 motion-safe:animate-bounce"
                 src={Logo}
                 alt="Loading Animation"
             />
+            
+            <p className="text-lg md:text-2xl">
+                {text || "Loading super awesome portal..."}
+            </p>
+         
+            <p className="text-md md:text-lg text-gray-500">
+                Please be paitent, <u> don't refresh! </u>
+            </p>
         </div>
     );
 };
