c7dadd3244909605b830ed052e1b317d029ca5d7
jc <46619361+juancwu@users.noreply.github.com>
2024-05-17 18:04:55 -0400

    remove volunteer check

--- src/navigation/ProtectedRoutes/ProtectedRoutes.tsx
+++ src/navigation/ProtectedRoutes/ProtectedRoutes.tsx
@@ -46,13 +46,7 @@ export const ProtectedRoutes: React.FC<ProtectedRoutesProps> = ({
         }
     }
 
-    if (
-        adminOnly &&
-        ((location.pathname.startsWith("/admin/ticket") &&
-            (session.currentUser.type !== "volunteer" ||
-                !session.currentUser.rsvpVerified)) ||
-            !session.currentUser.hawkAdmin)
-    ) {
+    if (adminOnly && !session.currentUser.hawkAdmin) {
         // redirect to not found page if user is not authorized to view
         return <Navigate to={routes.notFound} />;
     }
