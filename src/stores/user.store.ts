fa29d62374375379ae824350d5dd532f17d65418
jc <46619361+juancwu@users.noreply.github.com>
2024-05-16 23:27:12 -0400

    use user store to cache team data

--- src/stores/user.store.ts
+++ src/stores/user.store.ts
@@ -1,16 +1,36 @@
-import { Socials } from "@/services/utils/types";
+import { Socials, TeamData } from "@/services/utils/types";
 import { create } from "zustand";
 
 export interface UserStore {
     socials: Socials | null;
     setSocials: (socials: Socials | null) => void;
+
+    team: TeamData | null;
+    setTeam: (team: TeamData | null) => void;
+    updateTeamName: (name: string) => void;
 }
 
 export const useUserStore = create<UserStore>((set) => ({
+    // Socials
     socials: null,
     setSocials: (socials) =>
         set((state) => {
             state.socials = socials;
+            return state;
+        }),
+
+    // Team
+    team: null,
+    setTeam: (team) =>
+        set((state) => {
+            state.team = team;
+            return state;
+        }),
+    updateTeamName: (name: string) =>
+        set((state) => {
+            if (state.team) {
+                state.team.teamName = name;
+            }
             return state;
         }),
 }));
