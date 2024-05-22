135576589ddd0ef84ff8f63d8ca51f7ae549f19e
AmirAgassi <33383085+AmirAgassi@users.noreply.github.com>
2024-05-22 16:38:27 -0400

    Wait nah comment it out instead

--- src/providers/routes.provider.tsx
+++ src/providers/routes.provider.tsx
@@ -8,16 +8,47 @@ import {
 } from "react";
 import type { ComponentProps } from "@/components/types";
 import {
-    UserPage,
+    // AdminPage,
+    // LoginPage,
+    // NetworkingPage,
     NotFoundPage,
+    // TicketPage,
+    // HomePage,
+    // VerifyEmailPage,
+    UserPage,
+    // SchedulePage,
+    // PerksPage,
 } from "@/pages";
 import { type RouteObject } from "react-router-dom";
 import { useAuth } from "./auth.provider";
 import { ProtectedRoutes } from "@/navigation";
+// import { PostSubmissionPage } from "@/pages/miscellaneous/PostSubmission.page";
+// import { VerifyRSVP } from "@/pages/miscellaneous/VerifyRSVP.page";
+// import { MyTeamPage } from "@/pages/MyTeam.page";
+// import { ViewTicketPage } from "@/pages/miscellaneous/ViewTicket.page";
+// import { JoinTeamPage } from "@/pages/JoinTeam.page";
+// import { AdminViewTicketPage } from "@/pages/admin/ViewTicket.page";
+// import { AdminManageEventsPage } from "@/pages/admin/ManageEvents.page";
 
 interface PathObject {
+    // admin: string;
+    // adminViewTicket: string;
+    // adminManageEvents: string;
     notFound: string;
+    // login: string;
     portal: string;
+    // verifyEmail: string;
+    // schedule: string;
+    // networking: string;
+    // myTicket: string;
+    // application: string;
+    // submitted: string;
+    // verifyRSVP: string;
+    // myTeam: string;
+    // joinTeam: string;
+    // myApp: string;
+    // ticket: string;
+    // perks: string;
 }
 
 interface Title {
@@ -36,8 +67,24 @@ interface RoutesContextValue {
 
 // this path object provides a common place to define route pathnames
 const paths: PathObject = {
+    // admin: "/admin",
+    // adminViewTicket: "/admin/ticket/:ticketId",
+    // adminManageEvents: "/admin/manage",
     notFound: "*",
+    // login: "/login",
     portal: "/",
+    // verifyEmail: "/verify-email",
+    // schedule: "/schedule",
+    // networking: "/networking",
+    // myTicket: "/my-ticket",
+    // application: "/application",
+    // submitted: "/submitted",
+    // verifyRSVP: "/verify-rsvp",
+    // myTeam: "/my-team",
+    // joinTeam: "/join-team",
+    // myApp: "/my-application",
+    // ticket: "/ticket/:ticketId",
+    // perks: "/perks",
 };
 
 const titles: Record<string, Title> = {
@@ -45,6 +92,46 @@ const titles: Record<string, Title> = {
         main: "User",
         sub: "Welcome to your user dashboard.",
     },
+    // [paths.schedule]: {
+    //     main: "Schedule",
+    //     sub: "View the schedule for the weekend!",
+    // },
+    // [paths.networking]: {
+    //     main: "Networking",
+    //     sub: "A quick way to connect with new people at HawkHacks!",
+    // },
+    // [paths.application]: {
+    //     main: "Application",
+    //     sub: "Apply to participate in the hackathon now!",
+    // },
+    // [paths.verifyEmail]: {
+    //     main: "Verify Your Email",
+    //     sub: "Please check your email inbox.",
+    // },
+    // [paths.verifyRSVP]: {
+    //     main: "Verify Your RSVP",
+    //     sub: "All checkboxes are required.",
+    // },
+    // [paths.myTicket]: {
+    //     main: "Ticket",
+    //     sub: "This ticket is required for registration at our HawkHacks sign-in desk.\nKeep this ticket safe - download or add it to your wallet for convenience!",
+    // },
+    // [paths.myTeam]: {
+    //     main: "My Team",
+    //     sub: "Create your dream team! Add, manage, and view your teammates.",
+    // },
+    // [paths.joinTeam]: {
+    //     main: "Join Team",
+    //     sub: "Awesome, it looks like you have found teammates!",
+    // },
+    // [paths.ticket]: {
+    //     main: "View Ticket",
+    //     sub: "Some good thing here",
+    // },
+    // [paths.perks]: {
+    //     main: "Perks",
+    //     sub: "Explore the amazing perks available at HawkHacks!",
+    // },
 };
 
 const RoutesContext = createContext<RoutesContextValue>({
